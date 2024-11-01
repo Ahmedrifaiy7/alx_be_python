a4f825cbe6d11b053fab1bc10b29752c38a9f73c
Ahmed Ibrahim Amin Elrifaiy <rifaiyahmed@gmail.com>
2024-11-01 13:26:26 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,28 +1,15 @@
-from typing import Union
+# arithmetic_operations.py
 
-
-def perform_operation(num1: float, num2: float, operation: str) -> Union[float, str]:
-    """
-    Performs basic arithmetic operations based on the operation parameter.
-
-    Args:
-        num1 (float): The first numerical value.
-        num2 (float): The second numerical value.
-        operation (str): The operation to perform, one of 'add', 'subtract', 'multiply', or 'divide'.
-
-    Returns:
-        float: The result of the arithmetic operation.
-    """
-    if operation == 'add':
+def perform_operation(num1, num2, operation):
+    if operation == "add":
         return num1 + num2
-    elif operation == 'subtract':
+    elif operation == "subtract":
         return num1 - num2
-    elif operation == 'multiply':
+    elif operation == "multiply":
         return num1 * num2
-    elif operation == 'divide':
+    elif operation == "divide":
         if num2 == 0:
             return "Error: Division by zero is not allowed."
-        else:
-            return num1 / num2
+        return num1 / num2
     else:
-        return "Error: Invalid operation. Please use 'add', 'subtract', 'multiply', or 'divide'."
+        return "Error: Invalid operation."
